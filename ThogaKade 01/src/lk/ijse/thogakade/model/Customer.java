1d1d8ebdd6f2d141bd80257b3eee55e78e296b50
AquaKloud <133627006+AquaKloud@users.noreply.github.com>
2023-06-16 10:46:27 +0530

    Add files via upload

---
+++ ThogaKade 01/src/lk/ijse/thogakade/model/Customer.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lk.ijse.thogakade.model;
+
+/**
+ *
+ * @author LAHIRU SASANKA
+ */
+public class Customer {
+    private String cid;
+    private String name;
+    private String address;
+    private double salary;
+
+    public Customer() {
+    }
+
+    public Customer(String cid, String name, String address, double salary) {
+        this.cid = cid;
+        this.name = name;
+        this.address = address;
+        this.salary = salary;
+    }
+
+    /**
+     * @return the cid
+     */
+    public String getCid() {
+        return cid;
+    }
+
+    /**
+     * @param cid the cid to set
+     */
+    public void setCid(String cid) {
+        this.cid = cid;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the salary
+     */
+    public double getSalary() {
+        return salary;
+    }
+
+    /**
+     * @param salary the salary to set
+     */
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    
+    
+    
+    
+}
